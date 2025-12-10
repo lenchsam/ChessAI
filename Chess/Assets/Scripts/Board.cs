65ea36d8e23106f6201d66c2e4a98048446d0114
Sam <114984800+lenchsam@users.noreply.github.com>
2025-12-10 19:23:18 +0000

    starting positions
    uses the FEN system to put the pieces in the correct starting position

--- Chess/Assets/Scripts/Board.cs
+++ Chess/Assets/Scripts/Board.cs
@@ -1,18 +1,64 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    [SerializeField] private BoardSettings boardSettings;
-    [SerializeField] private GameObject squarePrefab;
+    [SerializeField] private BoardSettings _boardSettings;
+    [SerializeField] private GameObject _squarePrefab;
 
+    Dictionary<char, GameObject> _whitePieceDictionary;
+    Dictionary<char, GameObject> _blackPieceDictionary;
+
+    [Header("Piece Prefabs")]
+    [SerializeField] GameObject _whitePawnPrefab;
+    [SerializeField] GameObject _whiteRookPrefab;
+    [SerializeField] GameObject _whiteKnightPrefab;
+    [SerializeField] GameObject _whiteBishopPrefab;
+    [SerializeField] GameObject _whiteKingPrefab;
+    [SerializeField] GameObject _whiteQueenPrefab;
+
+    [Space(5)]
+
+    [SerializeField] GameObject _blackPawnPrefab;
+    [SerializeField] GameObject _blackRookPrefab;
+    [SerializeField] GameObject _blackKnightPrefab;
+    [SerializeField] GameObject _blackBishopPrefab;
+    [SerializeField] GameObject _blackKingPrefab;
+    [SerializeField] GameObject _blackQueenPrefab;
+
+    private GameObject _boardParent;
     private void Start()
     {
+        _boardParent = new GameObject();
+        _boardParent.name = "Board Squares";
         CreateBoard();
+
+        _whitePieceDictionary = new Dictionary<char, GameObject>() {
+        {'p', _whitePawnPrefab},
+        {'r', _whiteRookPrefab},
+        {'n', _whiteKnightPrefab},
+        {'b', _whiteBishopPrefab},
+        {'q', _whiteQueenPrefab},
+        {'k', _whiteKingPrefab}
+        };
+
+        _blackPieceDictionary = new Dictionary<char, GameObject>() {
+        {'p', _blackPawnPrefab},
+        {'r', _blackRookPrefab},
+        {'n', _blackKnightPrefab},
+        {'b', _blackBishopPrefab},
+        {'q', _blackQueenPrefab},
+        {'k', _blackKingPrefab}
+        };
+
+        //uppercase = white lowercase = black
+        FENtoPiecePositions("RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr");
+
     }
     void CreateBoard()
     {
-        Color whiteMaterial = boardSettings.whiteColor;
-        Color blackMaterial = boardSettings.blackColor;
+        Color whiteMaterial = _boardSettings.whiteColor;
+        Color blackMaterial = _boardSettings.blackColor;
         for (int x = 0; x < 8; x++)
         {
             for(int y = 0; y < 8; y++)
@@ -37,6 +83,59 @@ public class Board : MonoBehaviour
 
     void DrawSquare(Color colour, Vector2 position)
     {
-        Instantiate(squarePrefab, position, Quaternion.identity).GetComponent<SpriteRenderer>().color = colour;
+        Instantiate(_squarePrefab, position, Quaternion.identity, _boardParent.transform).GetComponent<SpriteRenderer>().color = colour;
+    }
+
+    void FENtoPiecePositions(string FEN)
+    {
+        int row = 0;
+        int col = 0;
+        int numEmpty = 0;
+        foreach (char c in FEN)
+        {
+            if (char.IsDigit(c))
+            {
+                //empty squares
+                int emptySquares = int.Parse(c.ToString());
+                numEmpty += emptySquares;
+                col += emptySquares;
+            }
+            else if (c == '/')
+            {
+                //new rank
+                row++;
+                numEmpty = 0;
+                col = 0;
+            }
+            else
+            {
+                //place pieces
+                if (numEmpty > 0)
+                {
+                    for (int i = 0; i < numEmpty; i++)
+                    {
+                        // Place empty square at (i + numEmpty, row)
+                    }
+                    numEmpty = 0;
+                }
+                else
+                {
+                    col++;
+                    // Place piece at (current file, row)
+                    Vector3 position = new Vector3(col - 1, row);
+                    Debug.Log($"Placing piece {c} at position {position}");
+                    // Instantiate piece based on character 'c' at position}
+                    Instantiate(CharToPiece(c), position, Quaternion.identity);
+
+                }
+            }
+        }
+    }
+    GameObject CharToPiece(char c)
+    {
+        bool isWhite = char.IsUpper(c);
+        char lower = char.ToLower(c);
+
+        return isWhite ? _whitePieceDictionary[lower] : _blackPieceDictionary[lower];
     }
 }
