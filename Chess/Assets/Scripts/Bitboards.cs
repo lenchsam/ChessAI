310e3cc5ee6750fcb5535cae5dcd9ab46fdd01f6
Sam <114984800+lenchsam@users.noreply.github.com>
2025-12-20 23:28:45 +0000

    Magic bitboards

--- Chess/Assets/Scripts/Bitboards.cs
+++ Chess/Assets/Scripts/Bitboards.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -28,6 +29,169 @@ public class Bitboards
     private ulong[] _whitePawnLookup = new ulong[64];
     private ulong[] _blackPawnLookup = new ulong[64];
     private ulong[] _kingLookup = new ulong[64];
+
+    //pre computed data for magic bitboards
+    private static readonly int[] _rookShifts = new int[64]
+    {
+        12, 11, 11, 11, 11, 11, 11, 12,
+        11, 10, 10, 10, 10, 10, 10, 11,
+        11, 10, 10, 10, 10, 10, 10, 11,
+        11, 10, 10, 10, 10, 10, 10, 11,
+        11, 10, 10, 10, 10, 10, 10, 11,
+        11, 10, 10, 10, 10, 10, 10, 11,
+        11, 10, 10, 10, 10, 10, 10, 11,
+        12, 11, 11, 11, 11, 11, 11, 12
+    };
+    private static readonly int[] _bishopShifts = new int[64]
+    {
+        6, 5, 5, 5, 5, 5, 5, 6,
+        5, 5, 5, 5, 5, 5, 5, 5,
+        5, 5, 7, 7, 7, 7, 5, 5,
+        5, 5, 7, 9, 9, 7, 5, 5,
+        5, 5, 7, 9, 9, 7, 5, 5,
+        5, 5, 7, 7, 7, 7, 5, 5,
+        5, 5, 5, 5, 5, 5, 5, 5,
+        6, 5, 5, 5, 5, 5, 5, 6
+    };
+    private static readonly ulong[] _rookMagics = new ulong[64]
+    {
+        0x8a80104000800020UL,
+        0x140002000100040UL,
+        0x2801880a0017001UL,
+        0x100081001000420UL,
+        0x200020010080420UL,
+        0x3001c0002010008UL,
+        0x8480008002000100UL,
+        0x2080088004402900UL,
+        0x800098204000UL,
+        0x2024401000200040UL,
+        0x100802000801000UL,
+        0x120800800801000UL,
+        0x208808088000400UL,
+        0x2802200800400UL,
+        0x2200800100020080UL,
+        0x801000060821100UL,
+        0x80044006422000UL,
+        0x100808020004000UL,
+        0x12108a0010204200UL,
+        0x140848010000802UL,
+        0x481828014002800UL,
+        0x8094004002004100UL,
+        0x4010040010010802UL,
+        0x20008806104UL,
+        0x100400080208000UL,
+        0x2040002120081000UL,
+        0x21200680100081UL,
+        0x20100080080080UL,
+        0x2000a00200410UL,
+        0x20080800400UL,
+        0x80088400100102UL,
+        0x80004600042881UL,
+        0x4040008040800020UL,
+        0x440003000200801UL,
+        0x4200011004500UL,
+        0x188020010100100UL,
+        0x14800401802800UL,
+        0x2080040080800200UL,
+        0x124080204001001UL,
+        0x200046502000484UL,
+        0x480400080088020UL,
+        0x1000422010034000UL,
+        0x30200100110040UL,
+        0x100021010009UL,
+        0x2002080100110004UL,
+        0x202008004008002UL,
+        0x20020004010100UL,
+        0x2048440040820001UL,
+        0x101002200408200UL,
+        0x40802000401080UL,
+        0x4008142004410100UL,
+        0x2060820c0120200UL,
+        0x1001004080100UL,
+        0x20c020080040080UL,
+        0x2935610830022400UL,
+        0x44440041009200UL,
+        0x280001040802101UL,
+        0x2100190040002085UL,
+        0x80c0084100102001UL,
+        0x4024081001000421UL,
+        0x20030a0244872UL,
+        0x12001008414402UL,
+        0x2006104900a0804UL,
+        0x1004081002402UL
+    };
+    private static readonly ulong[] _bishopMagics = new ulong[64]
+    {
+        0x40040844404084UL,
+        0x2004208a004208UL,
+        0x10190041080202UL,
+        0x108060845042010UL,
+        0x581104180800210UL,
+        0x2112080446200010UL,
+        0x1080820820060210UL,
+        0x3c0808410220200UL,
+        0x4050404440404UL,
+        0x21001420088UL,
+        0x24d0080801082102UL,
+        0x1020a0a020400UL,
+        0x40308200402UL,
+        0x4011002100800UL,
+        0x401484104104005UL,
+        0x801010402020200UL,
+        0x400210c3880100UL,
+        0x404022024108200UL,
+        0x810018200204102UL,
+        0x4002801a02003UL,
+        0x85040820080400UL,
+        0x810102c808880400UL,
+        0xe900410884800UL,
+        0x8002020480840102UL,
+        0x220200865090201UL,
+        0x2010100a02021202UL,
+        0x152048408022401UL,
+        0x20080002081110UL,
+        0x4001001021004000UL,
+        0x800040400a011002UL,
+        0xe4004081011002UL,
+        0x1c004001012080UL,
+        0x8004200962a00220UL,
+        0x8422100208500202UL,
+        0x2000402200300c08UL,
+        0x8646020080080080UL,
+        0x80020a0200100808UL,
+        0x2010004880111000UL,
+        0x623000a080011400UL,
+        0x42008c0340209202UL,
+        0x209188240001000UL,
+        0x400408a884001800UL,
+        0x110400a6080400UL,
+        0x1840060a44020800UL,
+        0x90080104000041UL,
+        0x201011000808101UL,
+        0x1a2208080504f080UL,
+        0x8012020600211212UL,
+        0x500861011240000UL,
+        0x180806108200800UL,
+        0x4000020e01040044UL,
+        0x300000261044000aUL,
+        0x802241102020002UL,
+        0x20906061210001UL,
+        0x5a84841004010310UL,
+        0x4010801011c04UL,
+        0xa010109502200UL,
+        0x4a02012000UL,
+        0x500201010098b028UL,
+        0x8040002811040900UL,
+        0x28000010020204UL,
+        0x6000020202d0240UL,
+        0x8918844842082200UL,
+        0x4010011029020020UL
+    };
+
+    private ulong[][] _rookAttackTable = new ulong[64][];
+    private ulong[][] _bishopAttackTable = new ulong[64][];
+    private ulong[] _rookMasks = new ulong[64];
+    private ulong[] _bishopMasks = new ulong[64];
 
     public void FENtoBitboards(string FEN)
     {
@@ -356,7 +520,6 @@ public class Bitboards
 
         return attacks;
     }
-
     private ulong CalculateBishopAttacks(int square, ulong blockers)
     {
         ulong attacks = 0;
@@ -399,7 +562,57 @@ public class Bitboards
         return attacks;
     }
 
+    public void InitialiseRookAndBishopMagics()
+    {
+        //allocate attack tables
+        _rookAttackTable = new ulong[64][];
+        _bishopAttackTable = new ulong[64][];
 
+        for (int square = 0; square < 64; square++)
+        {
+            //rook
+            _rookMasks[square] = MaskRookAttacks(square);
+            int rookRelevantBits = CountBits(_rookMasks[square]);
+            _rookShifts[square] = 64 - rookRelevantBits;
+
+            ulong[] rookBlockers = CreateAllBlockerBitboards(_rookMasks[square]);
+            _rookAttackTable[square] = new ulong[rookBlockers.Length];
+
+            for (int i = 0; i < rookBlockers.Length; i++)
+            {
+                ulong occupancy = rookBlockers[i];
+                int index = (int)((occupancy * _rookMagics[square]) >> _rookShifts[square]);
+                _rookAttackTable[square][index] = CalculateRookAttacks(square, occupancy);
+            }
+
+            //bishop
+            _bishopMasks[square] = MaskBishopAttacks(square);
+            int bishopRelevantBits = CountBits(_bishopMasks[square]);
+            _bishopShifts[square] = 64 - bishopRelevantBits;
+
+            ulong[] bishopBlockers = CreateAllBlockerBitboards(_bishopMasks[square]);
+            _bishopAttackTable[square] = new ulong[bishopBlockers.Length];
+
+            for (int i = 0; i < bishopBlockers.Length; i++)
+            {
+                ulong occupancy = bishopBlockers[i];
+                int index = (int)((occupancy * _bishopMagics[square]) >> _bishopShifts[square]);
+                _bishopAttackTable[square][index] = CalculateBishopAttacks(square, occupancy);
+            }
+        }
+    }
+
+    //Brian Kernighan’s algorithm
+    private int CountBits(ulong bb)
+    {
+        int count = 0;
+        while (bb != 0)
+        {
+            bb &= bb - 1; //clear the least significant bit set
+            count++;
+        }
+        return count;
+    }
 
     public void DebugBitboard(ulong bb, string label = "")
     {
@@ -544,35 +757,38 @@ public class Bitboards
     {
         ulong targetMask = 1UL << targetSquare;
 
-        ulong possibleAttacks = CalculateRookAttacks(startSquare, _allPiecesBB);
+        ulong occupancy = _allPiecesBB & _rookMasks[startSquare];
+        int index = (int)((occupancy * _rookMagics[startSquare]) >> _rookShifts[startSquare]);
 
-        if ((possibleAttacks & targetMask) == 0UL)
-            return false;
+        //lookup
+        ulong possibleAttacks = _rookAttackTable[startSquare][index];
+
+        if ((possibleAttacks & targetMask) == 0UL) return false;
 
         bool isWhite = (int)movingPiece <= (int)Piece.WhiteKing;
         ulong friendlyPieces = isWhite ? _whitePiecesBB : _blackPiecesBB;
+        bool isOccupiedByFriendly = (friendlyPieces & targetMask) == 0UL;
 
-        if ((friendlyPieces & targetMask) != 0UL)
-            return false;
-
-        return true;
+        return isOccupiedByFriendly;
     }
     private bool ValidateBishopMove(int startSquare, int targetSquare, Piece movingPiece)
     {
         ulong targetMask = 1UL << targetSquare;
 
-        ulong possibleAttacks = CalculateBishopAttacks(startSquare, _allPiecesBB);
+        ulong occupancy = _allPiecesBB & _bishopMasks[startSquare];
+        int index = (int)((occupancy * _bishopMagics[startSquare]) >> _bishopShifts[startSquare]);
 
-        if ((possibleAttacks & targetMask) == 0UL)
-            return false;
+        //lookup
+        ulong possibleAttacks = _bishopAttackTable[startSquare][index];
+
+        if ((possibleAttacks & targetMask) == 0UL) return false;
 
         bool isWhite = (int)movingPiece <= (int)Piece.WhiteKing;
         ulong friendlyPieces = isWhite ? _whitePiecesBB : _blackPiecesBB;
 
-        if ((friendlyPieces & targetMask) != 0UL)
-            return false;
+        bool isOccupiedByFriendly = (friendlyPieces & targetMask) == 0UL;
 
-        return true;
+        return isOccupiedByFriendly;
     }
 }
 
--- Chess/Assets/Scripts/GameManager.cs
+++ Chess/Assets/Scripts/GameManager.cs
@@ -27,8 +27,9 @@ public class GameManager : MonoBehaviour
         OnMoveRequested.AddListener(OnMoveRequestedHandler);
 
         //uppercase = white lowercase = black
-        BitboardScript.FENtoBitboards("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        BitboardScript.FENtoBitboards("r1bq1rk1/pp1n1ppp/2p2n2/3p4/3P4/2NBPN2/PP3PPP/R1BQ1RK1");
         BitboardScript.GenerateLookupTables();
+        BitboardScript.InitialiseRookAndBishopMagics();
 
         _board.CreateBoard();
         _board.DisplayPieces(BitboardScript);
